91d71b8d0dfd827039ffd3be201f1a2a94474faf
Viorel-Cosmin Miron <cosmin@uhl-services.ch>
2023-09-03 00:29:10 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ DEBIAN_FRONTEND=noninteractive
 
 # some mirrors have issues, i skipped httpredir in favor of an eu mirror
 
-echo "deb http://ftp.nl.debian.org/debian/ stretch main" > /etc/apt/sources.list
-echo "deb http://security.debian.org/debian-security stretch/updates main" >> /etc/apt/sources.list
+echo "deb http://ftp.nl.debian.org/debian/ buster main" > /etc/apt/sources.list
+echo "deb http://security.debian.org/debian-security buster/updates main" >> /etc/apt/sources.list
 
 # install dependencies for build
 # source: https://learn.netdata.cloud/docs/agent/packaging/installer/methods/manual
