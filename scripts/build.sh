b19e8abae1207c2aaaaf59c904236c3faa2ffb0b
Cosmic Sound <cosmin@uhl-services.ch>
2023-09-03 15:56:08 +0200

    Fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,9 +10,10 @@ echo "deb http://security.debian.org/debian-security bookworm-security main" >> 
 # install dependencies for build
 # source: https://learn.netdata.cloud/docs/agent/packaging/installer/methods/manual
 
-apt-get update && \
-apt-get install -y curl git && \
-curl -Ss 'https://raw.githubusercontent.com/netdata/netdata/master/packaging/installer/install-required-packages.sh' >/tmp/install-required-packages.sh && bash /tmp/install-required-packages.sh -i netdata-all
+
+apt-get update -qq && \
+apt-get install -y ca-certificates git-man krb5-locales less libbrotli1 libbsd0 libcbor0.8 libcurl3-gnutls libcurl4 libedit2 liberror-perl libexpat1 libfido2-1 libgdbm-compat4 libgdbm6 libgssapi-krb5-2 libk5crypto3 libkeyutils1 libkrb5-3 libkrb5support0 libldap-2.5-0 libldap-common libnghttp2-14 libperl5.36 libpsl5 librtmp1 libsasl2-2 libsasl2-modules libsasl2-modules-db libssh2-1 libssl3 libx11-6 libx11-data libxau6 libxcb1 libxdmcp6 libxext6 libxmuu1 netbase openssh-client openssl patch perl perl-modules-5.36 publicsuffix xauth && \
+apt-get install -y autoconf autoconf-archive autogen automake cmake curl g++ gcc git gzip libatomic1 libelf-dev libjson-c-dev liblz4-dev libmnl-dev libssl-dev libsystemd-dev libuv1-dev libyaml-dev lm-sensors make pkg-config python3 tar uuid-dev zlib1g-dev
 
 # fix the extra warning when building netdata
 
